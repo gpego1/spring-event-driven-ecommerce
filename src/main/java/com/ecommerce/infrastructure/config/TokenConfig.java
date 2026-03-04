4f4a5482ae1670a0e90abe496e9ee2c5e04326e1
gpego1 <gpego786@gmail.com>
2026-03-04 18:28:30 -0300

    feat: jwtToken

---
+++ src/main/java/com/ecommerce/infrastructure/config/TokenConfig.java
@@ -0,0 +1,17 @@
+//package com.ecommerce.infrastructure.config;
+//
+//import com.ecommerce.domain.entity.User;
+//import io.jsonwebtoken.Jwt;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class TokenConfig {
+//
+//    @Value("${JWT_SECRET}")
+//    private String JWT_SECRET;
+//
+//    private String generateToken(User user) {
+//
+//    }
+}
